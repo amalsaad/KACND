e949795e6419472acb3c8eb3360f257075f2244e
amalsaad <amal.al-sultan@hotmail.com>
2018-01-14 14:11:08 +0300

    project

---
+++ js/index.js
@@ -0,0 +1,10 @@
+﻿
+
+$("#submit").click(function (e) {
+   
+    $(".message-alert").html("");
+    if ($("#Username").val().length != 0 || $("#Password").val().length != 0) {
+        e.preventDefault();
+        $(".message-alert").append("<div class='alert alert-danger'><span> الرجاء تعبة الحقول المطلوبة * </span></div>");
+    }
+});
